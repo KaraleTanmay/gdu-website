fc79feff46d15e5bc7fb321be524ce11f1c81084
KaraleTanmay <107341046+KaraleTanmay@users.noreply.github.com>
2023-03-04 21:25:42 +0530

    game industry components removed

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Achievements from './components/achievements/Achievements'
-import GameIndustry from './components/game industry/GameIndustry'
 import Hero from './components/hero/Hero'
 import OurClub from './components/our club/OurClub'
 import OurGames from './components/our games/OurGames'
@@ -12,7 +11,6 @@ export default function Home() {
             home
             <Hero />
             <OurClub />
-            <GameIndustry />
             <Achievements />
             <OurGames />
             <OurRenders />
--- src/Components/home/components/game industry/GameIndustry.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function GameIndustry() {
-    return (
-        <div>
-            game industry
-        </div>
-    )
-}
